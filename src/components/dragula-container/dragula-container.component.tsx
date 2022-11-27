e0852c1c0db17b7c63fda288ad729641964530e5
Brandon Trabon <brandon.trabon@gmail.com>
2022-11-27 22:03:20 +1000

    Got the basic dragula service and dragula container written.

--- src/components/dragula-container/dragula-container.component.tsx
+++ src/components/dragula-container/dragula-container.component.tsx
@@ -1,5 +1,8 @@
-import React, { ReactNode } from 'react';
+import React, { memo, ReactNode, useEffect, useRef, useState } from 'react';
 import { v4 } from 'uuid';
+import { DragulaService } from '../../services/dragula.service';
+
+export type TypeWithKey<T> = T & { _key: string };
 
 export interface DragulaContainerProps<T> {
   containerName: string;
@@ -10,7 +13,7 @@ export interface DragulaContainerProps<T> {
   children: (item: T) => ReactNode;
 }
 
-export default function DragulaContainer<T>({
+function DragulaContainer<T>({
   containerName,
   copyItems = false,
   sortDirection = 'vertical',
@@ -18,16 +21,74 @@ export default function DragulaContainer<T>({
   onItemsChanged,
   children,
 }: DragulaContainerProps<T>) {
+  const [stateItems, setStateItems] = useState<Array<TypeWithKey<T>>>();
+
+  const currentItems = useRef<Array<TypeWithKey<T>>>();
+  const service = useRef<DragulaService>();
+  const containerElement = useRef<HTMLElement>();
+
+  useEffect(() => {
+    service.current = DragulaService.getServiceInstance();
+    service.current?.onDrop((element, target, source) => {
+      if (containerElement.current?.isSameNode(target)) {
+        reorganizeItems(target.children);
+      } else if (containerElement.current?.isSameNode(source)) {
+        reorganizeItems(source.children);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const itemsWithKey: Array<TypeWithKey<T>> = [];
+    for (let i = 0, length = items.length; i < length; i++) {
+      itemsWithKey.push({
+        ...items[i],
+        _key: v4(),
+      });
+    }
+
+    currentItems.current = itemsWithKey;
+    setStateItems(itemsWithKey);
+  }, [items]);
+
+  const reorganizeItems = (children: HTMLCollection) => {
+    // TODO: This will not work when an item is added
+    const newArray: Array<TypeWithKey<T>> = [];
+    for (let i = 0, length = children.length; i < length; i++) {
+      const child = children[i] as HTMLElement;
+      const foundItem = currentItems.current?.find((item) => item._key === child.dataset['id']);
+      if (foundItem) {
+        newArray.push(foundItem);
+      }
+    }
+
+    currentItems.current = newArray;
+    onItemsChanged?.(currentItems.current);
+  };
+
+  const onContainerCreated = (container: Element) => {
+    service.current?.addContainers([container]);
+    containerElement.current = container as HTMLElement;
+  };
+
   return (
-    <div data-drag-container={containerName} data-copy={copyItems} data-direction={sortDirection}>
-      {items.map((item) => {
-        const key = v4();
-        return (
-          <div key={key} id={key}>
-            {children(item)}
-          </div>
-        );
-      })}
+    <div
+      ref={(element) => element && onContainerCreated(element)}
+      data-drag-container={containerName}
+      data-copy={copyItems}
+      data-direction={sortDirection}
+    >
+      {stateItems &&
+        stateItems.map((item) => {
+          const { _key, ...itemNoKey } = item;
+          return (
+            <div key={_key} data-id={_key}>
+              {children(itemNoKey as T)}
+            </div>
+          );
+        })}
     </div>
   );
 }
+
+export default memo(DragulaContainer);
--- src/services/dragula.service.ts
+++ src/services/dragula.service.ts
@@ -4,7 +4,7 @@ export class DragulaService {
   private static instance: DragulaService;
 
   private drake: Drake;
-  private readonly options: DragulaOptions;
+  private options: DragulaOptions;
 
   private constructor(containers: Array<Element> = []) {
     this.options = {};
@@ -15,18 +15,52 @@ export class DragulaService {
     if (!DragulaService.instance) {
       DragulaService.instance = new DragulaService(containers);
     } else if (containers.length > 0) {
-      const currentContainers = DragulaService.instance.drake.containers;
-      const filteredContainers = containers.filter(
-        (container) =>
-          !currentContainers.some(
-            (currentContainer) =>
-              (currentContainer as HTMLElement).dataset['drag-container'] ===
-              (container as HTMLElement).dataset['drag-container']
-          )
-      );
-      currentContainers.push(...filteredContainers);
+      DragulaService.instance.addContainers(containers);
     }
 
     return DragulaService.instance;
+  }
+
+  public addContainers(containers: Array<Element>) {
+    const currentContainers = DragulaService.instance.drake.containers;
+    const filteredContainers = containers.filter(
+      (container) =>
+        !currentContainers.some(
+          (currentContainer) =>
+            (currentContainer as HTMLElement).dataset['drag-container'] ===
+            (container as HTMLElement).dataset['drag-container']
+        )
+    );
+    currentContainers.push(...filteredContainers);
+  }
+
+  public addOptionSetting(settings: DragulaOptions) {
+    this.options = {
+      ...this.options,
+      ...settings,
+    };
+  }
+
+  public onDrag(listener: (el: Element, source: Element) => void) {
+    this.drake.on('drag', listener);
+  }
+
+  public onDragEnd(listener: (el: Element) => void) {
+    this.drake.on('dragend', listener);
+  }
+
+  public onDrop(listener: (el: Element, target: Element, source: Element, sibling: Element) => void) {
+    this.drake.on('drop', listener);
+  }
+
+  public onCloned(listener: (clone: Element, original: Element, type: 'mirror' | 'copy') => void) {
+    this.drake.on('cloned', listener);
+  }
+
+  public on(
+    event: 'cancel' | 'remove' | 'shadow' | 'over' | 'out',
+    listener: (el: Element, container: Element, source: Element) => void
+  ) {
+    this.drake.on(event, listener);
   }
 }
